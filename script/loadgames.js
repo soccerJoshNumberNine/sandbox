5687e391f182f25d24dcf8d02d853c6f1734edee
Joshua Smith <blue@smittycraft.com>
2017-09-14 17:27:02 -0700

    Update loadgames.js

--- script/loadgames.js
+++ script/loadgames.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   var gamelist = JSON.parse(games);
   var i = 0;
   
-  for (i = 0; i < games.length; i++) {
+  for (i = 0; i < gamelist.length; i++) {
     $('#games').append('<tr><td>' + gamelist[i] + '</td></tr>');
   }
 });
